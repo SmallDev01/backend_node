6d851e6d8b09ec423dfbf740bf10936e824789ba
Adrian <acabanas@smalldev.es>
2022-02-23 13:00:00 +0100

    metiendo https

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // get driver connection
 const dbo = require("./db/conn");
 
